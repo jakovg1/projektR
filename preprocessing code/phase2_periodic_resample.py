15c77776ce4997a6f8f07bb114b1beb74129c6ec
Jakov Gracin <jakov.gracin1@gmail.com>
2021-01-17 20:09:57 +0100

    Revert "fixed paths"
    This reverts commit 64ab25ebc3ba91a68255f8c90aa5828f3f97b308.

--- preprocessing code/phase2_periodic_resample.py
+++ preprocessing code/phase2_periodic_resample.py
@@ -10,6 +10,9 @@ def execute(ext1, ext2):
 	#kod gdje uzima svaku petu sliku (no varijabla) iz originalnog dataseta i kopira ju u novi folder
 	# svaku petu sliku sam uzimala da bi se smanjio broj medusobno slicnih slika
 
+	ext1 = "Train"
+	ext2 = "train"
+
 	no = config.resample_rate
 
 	'''
