60b39706bd1c2cd1dcc8b514dfc0f1289e10afb3
Jakov Gracin <jakov.gracin1@gmail.com>
2021-01-18 21:00:58 +0100

    fixed indentation

--- preprocessing code/preprocess_config.py
+++ preprocessing code/preprocess_config.py
@@ -1,9 +1,16 @@
 class PreprocessConfig:
 	def __init__(self):
-	#====preprocessing configure
+		#====preprocessing configure
 		self.patch_size = 64
 		self.no_of_patches = 19
 		self.resample_rate = 4
 		
+		
+		
 		#MAKE SURE THESE PATHS IN "configure.py" and "preprocess_config.py" match!
-		self.path_short =	"C:/Users/Jakov/Documents/Misc/FER - jakov/5. semestar/Projekt R/Materijali - segm. srca/Git-Projekt R/notebooks/UNet/"
+		
+		#Google Colab
+		self.path_short ="/content/projektR/notebooks/UNet/"
+		
+		#Local
+		#self.path_short ="C:/Users/Jakov/Documents/Misc/FER - jakov/5. semestar/Projekt R/Materijali - segm. srca/Git-Projekt R/notebooks/UNet/"
